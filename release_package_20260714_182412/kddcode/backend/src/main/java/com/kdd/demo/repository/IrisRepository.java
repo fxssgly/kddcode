1578960f4c1bd9d819ac84d3e43838b0662055f2
gly <2710344405@qq.com>
2026-07-16 20:05:49 +0800

    修改代码

--- release_package_20260714_182412/kddcode/backend/src/main/java/com/kdd/demo/repository/IrisRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kdd.demo.repository;
-
-import com.kdd.demo.entity.IrisSample;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IrisRepository extends JpaRepository<IrisSample, Integer> {
-}
